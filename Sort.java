8497692b866152f8b86fbbfdff1e7517db2119cd
Aleksey <32610475+BeSt-Style-ViP@users.noreply.github.com>
2017-10-10 18:21:48 +0300

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,36 @@
+public class Sort {
+
+    public static void bubble(int[] src) {
+        for (int i = 0; i < src.length - 1; i++) {
+            boolean cont = false;
+            for (int x = 0; x < src.length - i - 1; x++) {
+                if (src[x] > src[x+1]) {
+                    int prevX = src[x];
+                    src[x] = src[x+1];
+                    src[x+1] = prevX;
+                    cont = true;
+                }
+            }
+            if (!cont)
+                break;
+        }
+        return;
+    }
+
+    public static void bubble(String[] src) {
+        for (int i = 0; i < src.length - 1; i++) {
+            boolean cont = false;
+            for (int x = 0; x < src.length - i - 1; x++) {
+                if (src[x].compareTo(src[x+1]) > 0) {
+                    String prevX = src[x];
+                    src[x] = src[x+1];
+                    src[x+1] = prevX;
+                    cont = true;
+                }
+            }
+            if (!cont)
+                break;
+        }
+        return;
+    }
+}
